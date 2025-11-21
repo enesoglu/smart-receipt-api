aa0af69ced2cf7e04d1f480d1644066b2565f2ce
Enes Yıldız <yildiz_enes@hotmail.com>
2025-11-21 21:41:11 +0300

    Add AppDbContext and database connection string
    Introduced AppDbContext for Entity Framework Core with DbSets for User and Receipt models. Updated appsettings.json to include DefaultConnection string for local SQL Server database.

---
+++ AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using smart_receipt_api.Models;
+
+namespace smart_receipt_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        // public DbSet<ReceiptItem> ReceiptItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // total amount of receipt
+            modelBuilder.Entity<Receipt>()
+                .Property(p => p.TotalAmount)
+                .HasColumnType("decimal(10,2)");
+
+            // price of item 
+            // modelBuilder.Entity<ReceiptItem>()
+            //    .Property(p => p.Price)
+            //    .HasColumnType("decimal(18,2)");
+        }
+    }
+}
