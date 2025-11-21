a965f587ff00b20ba12dab6c428e6da2a65fcb27
Enes Yıldız <yildiz_enes@hotmail.com>
2025-11-21 21:42:14 +0300

    Add Entity Framework Core and AppDbContext setup
    Configured the application to use Entity Framework Core with SQL Server by adding AppDbContext to the service container. This enables database access for the API.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using smart_receipt_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
